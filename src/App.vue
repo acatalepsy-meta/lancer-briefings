f4b566e8fb0aeade550ecbff48afd3fda7c641e8
Acatalepsy <acatalepsy.meta@gmail.com>
2022-09-08 20:00:41 -0400

    First pass at the Dragons of the Spine.

--- src/App.vue
+++ src/App.vue
@@ -89,68 +89,65 @@ export default {
 
   data() {
     return {
-      "mission_slug": "001",
-      "current_md": "",
+      "mission_slug": "002",
+      "current_md": "001",
       "events": "",
       "missions": [
         {
           "slug": "001",
           "name": "Bug-Hunt",
+          "status": "partial-success"
+        },
+		{
+          "slug": "002",
+          "name": "Vigilant-Gaze",
           "status": "start"
         },
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
+          "callsign": "Torrent",
+          "alias": "",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "corpro": "SSC",
+          "frame": "Monarch",
+          "mech": "Garuda"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Aspis",
+          "alias": "Von",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "corpro": "UNKNOWN",
+          "frame": "BALOR Pattern Group",
+          "mech": "Fly"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Braver",
+          "alias": "Genos",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "corpro": "IPS-N",
+          "frame": "Blackbeard",
+          "mech": "Savage"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
+          "callsign": "Stuntman",
+          "alias": "Gilles LeBlanc",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
-        },
-        {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
-          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rio Bravo"
+          "corpro": "IPS-N",
+          "frame": "Lancaster",
+          "mech": "Large"
         },
       ],
       "header": {
-        "planet": "Hercynia",
-        "year": "5014u",
-        "system": "Ardennes-3",
-        "gate": "Atlas-Quanokrim",
-        "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
-        "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
+        "planet": "Mnemosyne (Marble VII)",
+        "year": "5018u",
+        "system": "Mytikas",
+        "gate": "Cascade-Rainier",
+        "ring": "Cascade-Line",
+        "headerTitle": "Horizon",
+        "headerSubtitle": "Dragons of the Spine",
+        "subheaderTitle": "Cell Code",
+        "subheaderSubtitle": "Comet :: Peregrin :: Autumn",
       },
       "options":{
         "eventsMarkdownPerMission": true
